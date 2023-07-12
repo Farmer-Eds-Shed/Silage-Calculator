66d0e385c9c9900fa4f45074a9c8ea3a987bd8cc
Farmer-Eds-Shed <>
2023-07-12 13:07:24 +0000

    Tidy up lables

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,17 @@ google.charts.setOnLoadCallback(drawChart);
 
 function drawChart() {
     const data = google.visualization.arrayToDataTable([
-        ['Silage', 'Tonnes'],
-        ['Required', 55],
-        ['Available', 49]
+        ['Silage', 'Tonnes', { role: "style" }],
+        ['Required', 55, "green"],
+        ['Available', 49, "red"]
     ]);
 
     const options = {
-        title: 'Silage'
+        //title: 'Silage',
+        legend: 'none'
     };
+
+
 
     const chart = new google.visualization.BarChart(document.getElementById('myChart'));
     chart.draw(data, options);
