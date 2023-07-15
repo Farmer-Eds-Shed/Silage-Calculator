642fc621985f2d1fbf356ef24572c455299d5722
Farmer-Eds-Shed <>
2023-07-15 10:40:04 +0000

    Add Tabs for each section

--- assets/js/script.js
+++ assets/js/script.js
@@ -159,3 +159,18 @@ function drawChart() {
     const chart = new google.visualization.BarChart(document.getElementById('myChart'));
     chart.draw(data, options);
 }
+
+
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
